3422dfa2a0d9cd44352658635148270325893e3e
dneumann <dneumann42@proton.me>
2026-01-04 11:05:17 -0800

    wallust theming

---
+++ wallust/help.tcl
@@ -0,0 +1,11 @@
+array set theme {
+    base #171717
+    text #FBE8BA
+    heading #2A2A2A
+    heading_active #333333
+    selected_bg #3A3A3A
+    active_bg #2E2E2E
+    button_bg #2A2A2A
+    button_text #FBE8BA
+    selected_text #FBE8BA
+}
---
+++ wallust/templates/help.tcl
@@ -0,0 +1,11 @@
+array set theme {
+    base {{background}}
+    text {{foreground}}
+    heading {{background | lighten(0.08)}}
+    heading_active {{background | lighten(0.12)}}
+    selected_bg {{background | lighten(0.15)}}
+    active_bg {{background | lighten(0.1)}}
+    button_bg {{background | lighten(0.08)}}
+    button_text {{foreground}}
+    selected_text {{foreground}}
+}
